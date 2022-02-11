1d9808d8fce20e56c58460bcb962dfad508b004b
Youbin <ybin0108@gmail.com>
2022-02-11 10:20:40 +0900

    async & sync
    비동기, 동기 메서드

---
+++ file_system/asyncOrder.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+fs.readFile('./readme.txt', (err, data) => {
+    if (err) {
+        throw err;
+    }
+    console.log('1번', data.toString());
+    fs.readFile('./readme.txt', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        console.log('2번', data.toString());
+        fs.readFile('./readme.txt', (err, data) => {
+            if (err) {
+                throw err;
+            }
+            console.log('3번', data.toString());
+            fs.readFile('./readme.txt', (err, data) => {
+                if (err) {
+                    throw err;
+                }
+                console.log('4번', data.toString());
+})
