12a3d006525bb154cbce09c5474d9076f35a1e53
김유빈 <ybin0108@gmail.com>
2022-06-30 15:40:45 +0900

    models

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const sequelize = require('sequelize');
+const { SET_DEFERRED } = require('sequelize/types/deferrable');
+const { Sequelize } = require('../learn-sequelize/models');
+
+module.exports = class User extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            name: {
+                type: Sequelize.STRING(20),
+                allowNull: false,
+                unique: true,
+            },
+            age: {
+                type: Sequelize.INTEGER.UNSIGNED,
+                allowNull: false,
+            },
+            married: {
+                type: Sequelize.BOOLEAN, // true false
+                allowNull: false,
+            },
+            comment: {
+                type: Sequelize.TEXT,
+                allowNull: true,
+            },
+            created_at: {
+                type: Sequelize.DATE, // DATETIME, MYSQL DATE -> Sequelize DateOnly
+                allowNull: false,
+                defaultValue: Sequelize.NOW,
+            },
+        }, {
+            sequelize,
+            timestamps: false,
+            underscored: false,
+            modelName: 'User',
+            tableName: 'users',
+            paranoid: false,
+            charset: 'utf8',
+            collate: 'utf8_general_ci'
+        });
+    }
+    static associate(db) {}
+};
